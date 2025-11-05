3bea43bc447a013f0523a4348a192e41521202c4
Artuur838 <artuur.neirynck@icloud.com>
2025-11-05 19:33:17 +0100

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,1 +1,10 @@
+from . import db
 
+class WEBUSER(db.Model):
+    __tablename__ = 'WEBUSER'  # moet exact overeenkomen met de tabelnaam in Supabase (kleine letters aanbevolen)
+    
+    WEBUSER_ID = db.Column(db.Integer, primary_key=True)
+    Name = db.Column(db.String(100), unique=True, nullable=False)
+
+    def __repr__(self):
+        return f'<WEBUSER {self.Name}>'
